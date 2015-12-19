374259d591c4cd46236f101c7a05a4ddcb39cf97
Ashish <Ashish@ASHISH-PC.Home>
2015-12-19 06:14:40 -0700

    initial commit

---
+++ TestUpload/src/demo/DemoSysOut.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package demo;
+
+/**
+ * @author Ashish
+ *
+ */
+public class DemoSysOut {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Demo Sysout");
+	}
+
+}
