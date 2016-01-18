d8c98f3396c66e350d8d050a6c83d739b8122c18
Ashish <Ashish@ASHISH-PC.Home>
2016-01-17 23:37:02 -0700

    commit

--- messenger/src/main/java/org/ashish/restful/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/ashish/restful/messenger/resources/MessageResource.java
@@ -5,7 +5,11 @@ package org.ashish.restful.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -25,17 +29,41 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		return messageService.addMessage(message);
+	}
+	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") Long messageId){
 		return messageService.getMessage(messageId);
 		
 	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") Long messageId,Message message){
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("messageId") Long messageId){
+		messageService.removeMessage(messageId);
+	}
+	
 
 }
